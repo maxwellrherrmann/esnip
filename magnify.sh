bc5d6663c44aa732d3e2f50d5b2e4dcce902644a
maxwellrherrmann <maxwellrherrmann@gmail.com>
2023-08-18 15:36:49 -0600

    Functional now

--- magnify.sh
+++ magnify.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "This is just for the first commit"
+
+# Make sure to change the target directory for the screenshot
+tempdir=/home/${USER}/Pictures/Screenshots/tmp/
+
+# Give random unique file name
+filename=${tempdir}$(echo $RANDOM | md5sum | head -c 10).png
+
+# Calls escrotum with -s to select region
+escrotum -s ${filename}
+
+# Opens the file in feh
+feh ${filename} 
+
+# Deletes file (it's intended to be temporary)
+rm ${filename}
