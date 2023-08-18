54d6fa313dc6c94e584945789769c9897a179801
maxwellrherrmann <maxwellrherrmann@gmail.com>
2023-08-18 15:38:05 -0600

    Made the 'temporary directory' just /tmp

--- magnify.sh
+++ magnify.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Make sure to change the target directory for the screenshot
-tempdir=/home/${USER}/Pictures/Screenshots/tmp/
-
 # Give random unique file name
-filename=${tempdir}$(echo $RANDOM | md5sum | head -c 10).png
+filename=/tmp/$(echo $RANDOM | md5sum | head -c 10).png
 
 # Calls escrotum with -s to select region
 escrotum -s ${filename}
