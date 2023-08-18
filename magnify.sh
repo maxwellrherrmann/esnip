19589852b4d7abcfb2d581b952d04ba6ad158ba7
maxwellrherrmann <maxwellrherrmann@gmail.com>
2023-08-18 16:07:52 -0600

    Now checks for escrotum and feh, exits with error if not found

--- magnify.sh
+++ magnify.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Checks to see that escrotum and feh are installed
+command -v escrotum >/dev/null 2>&1 || { echo >&2 "escrotum not found.  Aborting."; exit 1; }
+command -v feh >/dev/null 2>&1 || { echo >&2 "feh not found.  Aborting."; exit 1; }
+
 # Give random unique file name
 filename=/tmp/$(echo $RANDOM | md5sum | head -c 10).png
 
